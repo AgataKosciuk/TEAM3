fa503c86df1ec3a6991b4a8e64c2794f09875f43
KalinaPodyplomowe <pgx17581@student.wsb.gda.pl>
2023-02-17 19:42:24 +0100

    Initial

---
+++ program.py
@@ -0,0 +1,16 @@
+# wersja 1.0
+wiek = input("Podaj wiek: ")
+
+if wiek.isdigit() == False:
+    exit("Wiek musi być liczbą całkowitą")
+wiek = int(wiek)
+
+if wiek >= 18 and wiek <= 40:
+    print("Witaj w naszym sklepie")
+
+elif wiek >= 40 and wiek <= 100:
+    print("Witaj w naszym sklepie")
+    print("W Twoim wieku alkohol jest niewskazany")
+
+else:
+    print("Jesteś za małody. Zapraszamy na Disney.com")
